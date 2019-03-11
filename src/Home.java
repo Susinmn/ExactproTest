58a161d4442f03d82dadcde74b9b1b7362ba6db0
Susinmn <susinmn@mail.ru>
2019-03-11 12:57:00 +0400

    For Exactpro

--- src/Home.java
+++ src/Home.java
@@ -11,3 +11,39 @@ interface Encoder {
     byte[] serialize(Object anyBean) throws InvalidClassException;
     Object deserialize(byte[] data) throws ClassNotFoundException, InvalidClassException, StreamCorruptedException;
 }
+
+public class Home implements Encoder {
+
+    @Override
+    public byte[] serialize(Object anyBean) throws InvalidClassException {
+
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(anyBean);
+
+            return byteArrayOutputStream.toByteArray();
+
+        } catch (IOException e) {
+
+            return null;
+
+        }
+
+    }
+
+    @Override
+    public Object deserialize(byte[] data) throws ClassNotFoundException, InvalidClassException, StreamCorruptedException {
+
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data)) {
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            return objectInputStream.readObject();
+
+        } catch (IOException e) {
+            
+            return null;
+
+        }
+    }
+}
